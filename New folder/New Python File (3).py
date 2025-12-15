0c59cb9a8aab46d5ea79593cdeaf118c8720dc2e
Nkhai-MeoMeo <nkha8283@gmail.com>
2025-12-15 14:12:11 +0700

    HIHI

---
+++ New folder/New Python File (3).py
@@ -0,0 +1,3 @@
+n = int(input("Nhập  số n :  "))
+for n in range(1 , n+1):
+    print(n)
---
+++ New folder/New Python File.py
@@ -0,0 +1,7 @@
+a = int(input("Nhập số a : "))
+b = int(input("Nhập số b : "))
+c = (a + b)  * 2 
+print(f"Chu vi {c}")
+s =  a + b
+print(f"Diện  tích {s}")
+
