d079d1db345a89a3a20300080fe56f2d0b89a2cb
Nguyen_Le_Hoang_Khai <nkha8283@gmail.com>
2025-12-29 14:36:53 +0700

    Update TH.py

--- TH.py
+++ TH.py
@@ -5,9 +5,10 @@ c = float(input("điểm Hóa : "))
 diem_tong = (a + b + c) 
 if diem_tong >= 15 and diem_tong != 4 :
     print("Đậu")
-    if diem_tong > 5:
+    if a > 5 and b > 5 and c > 5:
         print("Học đều các môn")
     else:
         print("Học chưa đều các môn")
 else:
+
     print("Thi hỏng")
